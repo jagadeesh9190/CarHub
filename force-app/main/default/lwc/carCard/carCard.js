795d3b02fa26c5328ba10ee638c61856cf3a89fd
Jagadeesh Tirumalasetti <jagadeesh.tirumalasetti@genesys.com>
2024-04-17 02:50:49 -0400

    Initial commit

--- force-app/main/default/lwc/carCard/carCard.js
+++ force-app/main/default/lwc/carCard/carCard.js
@@ -1,73 +1,81 @@
-/* import { LightningElement, wire } from 'lwc';
-//getFiledValue is used to extract the field values 
-import { getFieldValue } from 'lightning/uiRecordApi'
+import { LightningElement, wire } from 'lwc';
 
+//Navigation
+import {NavigationMixin} from 'lightning/navigation'
 
-// car__c schema 
-import NAME_FILED from '@salesforce/schema/Car__c.Name'
-import PICTURE_URL_FILED from '@salesforce/schema/Car__c.Picture_URL__c'
+//Car__c Schema
+import CAR_OBJECT from '@salesforce/schema/Car__c'
+import NAME_FIELD from '@salesforce/schema/Car__c.Name'
+import PICTURE_URL_FIELD from '@salesforce/schema/Car__c.Picture_URL__c'
 import CATEGORY_FIELD from '@salesforce/schema/Car__c.Category__c'
 import MAKE_FIELD from '@salesforce/schema/Car__c.Make__c'
+import MSRP_FIELD from '@salesforce/schema/Car__c.MSRP__c'
 import FUEL_FIELD from '@salesforce/schema/Car__c.Fuel_Type__c'
 import SEATS_FIELD from '@salesforce/schema/Car__c.Seats__c'
 import CONTROL_FIELD from '@salesforce/schema/Car__c.Control__c'
-import MSRP_FIELD from '@salesforce/schema/Car__c.MSRP__c'
+// getFieldValue function is used to extract field values
+import {getFieldValue} from 'lightning/uiRecordApi'
 
+//lightning message service
+import {subscribe, MessageContext, unsubscribe} from 'lightning/messageService'
+import CAR_SELECTED_MESSAGE from '@salesforce/messageChannel/CarSelected__c'
 
-export default class CarCard extends LightningElement {
+export default class CarCard extends NavigationMixin(LightningElement) {
 
-    //Exposing fields to make available in the template  
+    /// load content for LMS
+    @wire(MessageContext)
+    messageContext
+
+    //exposing fields to make them available in the template
     categoryField = CATEGORY_FIELD
-    makeField = MAKE_FIELD
-    fuelField = FUEL_FIELD
-    SeatsField = SEATS_FIELD
-    ControlField = CONTROL_FIELD
+    makeField = MAKE_FIELD 
     msrpField = MSRP_FIELD
+    fuelField = FUEL_FIELD
+    seatsField = SEATS_FIELD
+    controlField = CONTROL_FIELD
 
-    recordId = 'a082w00000viHrRAAU'
-    //Car Fields display with specific format
+    //Id of Car__c to display data
+    recordId
+
+    // car fields displayed with specific format
     carName
     carPictureUrl
-    handleRecordLoaded(event) {
-        const { records } = event.detail
-        const recordDate = records.this.recordId
-        this.carName = getFieldValue(recordDate, NAME_FILED)
-        this.carPictureUrl = getFieldValue(recordDate, PICTURE_URL_FILED)
+
+    //subscription reference for carSelected
+    carSelectionSubscription 
+
+    handleRecordLoaded(event){
+        const {records} = event.detail
+        const recordData = records[this.recordId]
+        this.carName = getFieldValue(recordData, NAME_FIELD)
+        this.carPictureUrl = getFieldValue(recordData, PICTURE_URL_FIELD)
     }
-} */
-import { LightningElement } from "lwc";
 
-//Car__c Schema
+    connectedCallback(){
+        this.subscribeHandler()
+    }
 
-import NAME_FIELD from "@salesforce/schema/Car__c.Name";
-import PICTURE_URL_FIELD from "@salesforce/schema/Car__c.Picture_URL__c";
-import CATEGORY_FIELD from "@salesforce/schema/Car__c.Category__c";
-import MAKE_FIELD from "@salesforce/schema/Car__c.Make__c";
-import MSRP_FIELD from "@salesforce/schema/Car__c.MSRP__c";
-import FUEL_FIELD from "@salesforce/schema/Car__c.Fuel_Type__c";
-import SEATS_FIELD from "@salesforce/schema/Car__c.Seats__c";
-import CONTROL_FIELD from "@salesforce/schema/Car__c.Control__c";
-// getFieldValue function is used to extract field values
-import { getFieldValue } from "lightning/uiRecordApi";
-export default class CarCard extends LightningElement {
-  //exposing fields to make them available in the template
-  categoryField = CATEGORY_FIELD;
-  makeField = MAKE_FIELD;
-  msrpField = MSRP_FIELD;
-  fuelField = FUEL_FIELD;
-  seatsField = SEATS_FIELD;
-  controlField = CONTROL_FIELD;
+    subscribeHandler(){
+        this.carSelectionSubscription = subscribe(this.messageContext, CAR_SELECTED_MESSAGE, (message)=>this.handleCarSelected(message))
+    }
+    handleCarSelected(message){
+        this.recordId = message.carId
+    }
 
-  //Id of Car__c to display data
-  recordId = "a082w00000viHrRAAU";
+    disconnectedCallback(){
+        unsubscribe(this.carSelectionSubscription)
+        this.carSelectionSubscription = null
+    }
 
-  // car fields displayed with specific format
-  carName;
-  carPictureUrl;
-  handleRecordLoaded(event) {
-    const { records } = event.detail;
-    const recordData = records[this.recordId];
-    this.carName = getFieldValue(recordData, NAME_FIELD);
-    this.carPictureUrl = getFieldValue(recordData, PICTURE_URL_FIELD);
-  }
+    /**navigate to record page */
+    handleNavigateToRecord(){
+        this[NavigationMixin.Navigate]({
+            type:'standard__recordPage',
+            attributes:{
+                recordId:this.recordId,
+                objectApiName:CAR_OBJECT.objectApiName,
+                actionName:'view'
+            }
+        })
+    }
 }
